fe0aae64436c4b2a88575ba8ec93267431cf63fe
Alejandro Fernandez-Fraga <a.fernandez3@udc.es>
2024-03-12 10:29:29 +0100

    modified tests

--- scripts/ft3/exec/exec-ft3-a100-cuda-big.sh
+++ scripts/ft3/exec/exec-ft3-a100-cuda-big.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH -J output/cuda_exec_beta_pdf_A100
+#SBATCH -J output/cuda_big_exec_beta_pdf_A100
 #SBATCH -o %x-%j.out
 #SBATCH -c 32
 #SBATCH --mem-per-cpu=3G
@@ -20,7 +20,7 @@ EXECUTABLE=../bin/bench_base
 # run the script for different values of parametrs
 # for examples num elements = 10000000, 100000000 and 1000000000
 
-for i in 10000000 100000000 1000000000
+for i in 1000000000
 do
     for j in cuda cuda_f
     do
--- scripts/ft3/exec/exec-ft3-a100.sh
+++ scripts/ft3/exec/exec-ft3-a100.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #SBATCH -J output/exec_beta_pdf_A100
 #SBATCH -o %x-%j.out
-#SBATCH -c 32
-#SBATCH --mem-per-cpu=4G
-#SBATCH --gres=gpu:a100:1
-#SBATCH -t 5:00
+#SBATCH -c 64
+#SBATCH --mem-per-cpu=3G
+#SBATCH --gres=gpu:a100:2
+#SBATCH -t 55:00
 
 module load cesga/2020 cuda/12.2.0
 cd build
@@ -22,7 +22,7 @@ EXECUTABLE=../bin/bench_base
 
 for i in 10000000 100000000 1000000000
 do
-    for j in seq omp cuda cuda_f
+    for j in cuda cuda_f omp seq
     do
         OMP_NUM_THREADS=32 $EXECUTABLE $i 3 $j betapdf
     done
