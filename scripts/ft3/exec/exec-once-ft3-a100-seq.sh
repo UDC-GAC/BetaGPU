ed93f3679cd4ee8890739d5b1b0804f9672f1c58
Alejandro Fernandez-Fraga <a.fernandez3@udc.es>
2024-03-19 12:22:15 +0100

    added scripts for only one execution per run

---
+++ scripts/ft3/exec/exec-once-ft3-a100-seq.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#SBATCH -J output/seq_beta_pdf_A100
+#SBATCH -o %x-%j.out
+#SBATCH -c 32
+#SBATCH --mem-per-cpu=3G
+#SBATCH --gres=gpu:a100:1
+#SBATCH -t 55:00
+
+module load cesga/2020 cuda/12.2.0
+cd build
+cmake -DCMAKE_BUILD_TYPE=Release ..
+sleep 1
+make clean
+sleep 1
+make VERBOSE=1
+sleep 1
+
+EXECUTABLE=../bin/bench_base
+
+# run the script for different values of parametrs
+# for examples num elements = 10000000, 100000000 and 1000000000
+
+for i in 10000000 100000000 1000000000
+do
+    for j in seq
+    do
+        for n in {1..5}
+        do
+            OMP_NUM_THREADS=32 $EXECUTABLE $i 1 $j betapdf
+        done
+    done
+done
