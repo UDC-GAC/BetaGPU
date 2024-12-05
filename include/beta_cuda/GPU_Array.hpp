29a59736cf10af638d4fa90c5292f487e4c81dd5
Alejandro Fernandez-Fraga <a.fernandez3@udc.es>
2024-12-05 14:38:16 +0100

    added Rule of 5 to GPU_Array class

--- include/beta_cuda/GPU_Array.hpp
+++ include/beta_cuda/GPU_Array.hpp
@@ -14,16 +14,23 @@ public:
     GPU_Array();
     GPU_Array(size_t size);
     GPU_Array(const T *data, size_t size);
+    
+    // Rule of 5
     ~GPU_Array();
+    GPU_Array(const GPU_Array &other); // Copy constructor
+    GPU_Array(GPU_Array &&other) noexcept; // Move constructor
+    GPU_Array &operator=(const GPU_Array &other); // Copy assignment
+    GPU_Array &operator=(GPU_Array &&other) noexcept; // Move assignment
+
 
 
     const T *get_device_data();
     T *device_data();
-    void set_device_data(const T *data, size_t size, bool copy=true);
+    void set_device_data(const T *data, size_t size, bool copy_to_host=true);
 
     const T *get_host_data();
     T *host_data();
-    void set_host_data(const T *data, size_t size, bool copy=true);
+    void set_host_data(const T *data, size_t size, bool copy_to_device=true);
 
     size_t get_size();
     MemoryState get_memory_state();
@@ -38,5 +45,6 @@ private:
     void copy_to_device(size_t elements);
     void copy_to_host(size_t elements);
     void allocate(size_t elements);
+    void free();
 
 };
--- src/src_cuda/GPU_Array.cu
+++ src/src_cuda/GPU_Array.cu
@@ -11,18 +11,67 @@ GPU_Array<T>::GPU_Array(size_t size): size(size), memory_state(MemoryState::IN_S
 }
 
 template <typename T>
-GPU_Array<T>::GPU_Array(const T *data, size_t size): size(size), memory_state(MemoryState::IN_SYNC){
+GPU_Array<T>::GPU_Array(const T *data, size_t size): size(size), memory_state(MemoryState::IN_SYNC) {
     this->allocate(size);
     cudaMemcpy(this->data_h, data, size * sizeof(T), cudaMemcpyHostToHost);
     cudaMemcpy(this->data_d, data, size * sizeof(T), cudaMemcpyHostToDevice);
 }
 
+// Rule of 5
+
+// I.-   Destructor
 template <typename T>
 GPU_Array<T>::~GPU_Array(){
-    if (this->data_d != nullptr)
-        cudaFree(this->data_d);
-    if (this->data_h != nullptr)
-        cudaFreeHost(this->data_h);
+    this->free();
+}
+
+// II.-  Copy constructor
+template <typename T>
+GPU_Array<T>::GPU_Array(const GPU_Array &other): size(other.size), memory_state(other.memory_state){
+    this->allocate(other.size);
+    if (other.memory_state == MemoryState::HOST_ONLY || other.memory_state == MemoryState::IN_SYNC)
+        cudaMemcpy(this->data_h, other.data_h, other.size * sizeof(T), cudaMemcpyHostToHost);
+    if (other.memory_state == MemoryState::DEVICE_ONLY || other.memory_state == MemoryState::IN_SYNC)
+        cudaMemcpy(this->data_d, other.data_d, other.size * sizeof(T), cudaMemcpyDeviceToDevice);
+}
+
+// III.- Copy assignment
+template <typename T>
+GPU_Array<T> &GPU_Array<T>::operator=(const GPU_Array &other){
+
+    if (this == &other)
+        return *this;
+
+    this->size = other.size;
+    this->memory_state = other.memory_state;
+    this->allocate(other.size);
+    if (other.memory_state == MemoryState::HOST_ONLY || other.memory_state == MemoryState::IN_SYNC)
+        cudaMemcpy(this->data_h, other.data_h, other.size * sizeof(T), cudaMemcpyHostToHost);
+    if (other.memory_state == MemoryState::DEVICE_ONLY || other.memory_state == MemoryState::IN_SYNC)
+        cudaMemcpy(this->data_d, other.data_d, other.size * sizeof(T), cudaMemcpyDeviceToDevice);
+
+    return *this;
+}
+
+// IV.-  Move constructor
+template <typename T>
+GPU_Array<T>::GPU_Array(GPU_Array &&other) noexcept
+    : size(other.size), data_d(std::exchange(other.data_d, nullptr)), 
+      data_h(std::exchange(other.data_h, nullptr)), memory_state(other.memory_state){}
+
+// V.-   Move assignment
+template <typename T>
+GPU_Array<T> &GPU_Array<T>::operator=(GPU_Array &&other) noexcept {
+    if (this == &other)
+        return *this;
+
+    this->size = other.size;
+    this->memory_state = other.memory_state;
+    std::swap(this->data_d, other.data_d);
+    tstd::swap(this->data_h, other.data_h);
+    
+
+    return *this;
 }
 
 template <typename T>
@@ -43,11 +92,11 @@ T *GPU_Array<T>::device_data(){
 }
 
 template <typename T>
-void GPU_Array<T>::set_device_data(const T *data, size_t size, bool copy){
+void GPU_Array<T>::set_device_data(const T *data, size_t size, bool copy_to_host){
     cudaMemcpy(this->data_d, data, size * sizeof(T), cudaMemcpyDeviceToDevice);
     this->memory_state = MemoryState::DEVICE_ONLY;
 
-    if (copy){
+    if (copy_to_host){
         this->copy_to_host(size);
         this->memory_state = MemoryState::IN_SYNC;
     }
@@ -71,11 +120,11 @@ T *GPU_Array<T>::host_data(){
 }
 
 template <typename T>
-void GPU_Array<T>::set_host_data(const T *data, size_t size, bool copy){
+void GPU_Array<T>::set_host_data(const T *data, size_t size, bool copy_to_device){
     cudaMemcpy(this->data_h, data, size * sizeof(T), cudaMemcpyHostToHost);
     this->memory_state = MemoryState::HOST_ONLY;
 
-    if (copy){
+    if (copy_to_device){
         this->copy_to_device(size);
         this->memory_state = MemoryState::IN_SYNC;
     }
@@ -106,16 +155,27 @@ void GPU_Array<T>::copy_to_host(size_t elements){
 }
 
 template <typename T>
-void GPU_Array<T>::allocate(size_t elements){
-    if (this -> data_d != nullptr){
+void GPU_Array<T>::free(){
+    if (this->data_d != nullptr)
         cudaFree(this->data_d);
-    }
-    if (this -> data_h != nullptr){
+    if (this->data_h != nullptr)
         cudaFreeHost(this->data_h);
-    }
+
+    this->data_d = nullptr;
+    this->data_h = nullptr;
+}
+
+template <typename T>
+void GPU_Array<T>::allocate(size_t elements){
+    this->free();
     cudaMalloc(&this->data_d, elements * sizeof(T));
     cudaMallocHost(&this->data_h, elements * sizeof(T));
+    if (...){
+        this->free();
+        throw std::bad_alloc();
+    }
 }
+
 
 template class GPU_Array<int>;
 template class GPU_Array<float>;
